aec00c81703f82cee4438c819cc97d7398f94712
statevector <cwillis@cern.ch>
2021-09-30 02:13:19 -0600

    added binomial example

--- binomial.py
+++ binomial.py
@@ -11,60 +11,57 @@ def callback(x):
 	print('{}	{}'.format(Nfeval, x))
 	Nfeval += 1
 
-def poisson(x, args):
+def binomial(x, args):
 	# Calculate the negative log likelihood
 	k = args
 	if x <= 0:
 		x = 1e-12
-	nLL = -(np.sum(k)*np.log(x) - len(k)*x)
+	nLL = -(np.sum(k)*np.log(x) + (len(k)-np.sum(k))*np.log(1-x))
 	#print('nLL: {}'.format(nLL))
 	return nLL
 
 if __name__ == '__main__':
 
-	# generate poisson distributed data with \lambda = 3
-	# we want to identify \lambda using numeric MLE
+	# generate bernoulli distributed data with p = 0.2
+	# we want to identify p using numeric MLE
 	n = 1000
-	sample_data = np.random.poisson(lam=3, size=n)
+	sample_data = np.random.binomial(n=1, p=0.2, size=n)
 	print('data: {}'.format(sample_data))
 
-	# analytic form of \lambda from algebraic MLE
-	# \lambda = \sum(k_i)/n
-	lam = np.sum(sample_data)/n
-	print('lambda: {}'.format(lam))
+	# analytic form of p from algebraic MLE
+	p = np.sum(sample_data)/n
+	print('p: {}'.format(p))
 
-	# initial guess for \lambda
-	x0 = 1
-	nll = poisson(x0, sample_data)
-	print('nLL(data|lambda=1): {}'.format(nll))
+	# initial guess for p
+	x0 = 0.5
+	nll = binomial(x0, sample_data)
+	print(nll)
 
 	# minimize the negative log-likelihood
-	result = optimize.minimize(fun = poisson, 
+	result = optimize.minimize(fun = binomial, 
 		x0 = x0, 
 		args = sample_data, 
 		method = 'L-BFGS-B',
-		#bounds = ((0, 10),),
+		# bounds = ((0, 1),),
 		callback = callback,
 		options = {'maxiter': 10000, 'disp': True})
 
 	print('Solution: x = {}'.format(result.x))
 
 	# plot the nLL
-	x = np.arange(1, 6, 0.1)
+	x = np.arange(0, 1, 0.01)
 	y = []
 	for x0 in x:
-		val = poisson(x0, sample_data)
+		val = binomial(x0, sample_data)
 		y.append(val)
 
 	import matplotlib.pyplot as plt
 	plt.plot(x, y, 'r-', label='nLL(x)')
-	plt.vlines(result.x[0], -500, 1000, linestyle='--', label='x0')
-	plt.ylim([-500, 1000])
+	plt.vlines(result.x[0], 0, 2000, linestyle='--', label='x0')
+	plt.ylim([0, 2000])
 	plt.title('Negative Log-Likelihood')
 	plt.xlabel('x')
 	plt.ylabel('nLL(x)')
 	plt.legend(loc='best')
-	#plt.savefig('curve.pdf')
 	plt.show()
-
 
